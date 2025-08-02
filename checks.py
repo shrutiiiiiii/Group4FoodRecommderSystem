0ab683f5c2aadc6534086b5e5b1880a9d4e82d06
Sunil Dutt <sunil.dutt28@gmail.com>
2025-08-02 11:52:20 +0100

    changed to reference path

--- checks.py
+++ checks.py
@@ -1,4 +1,4 @@
 import onnx
-model = onnx.load("C:/Users/Bazam/Documents/DBS/SECOND SEMESTER/RECCOMENDER SYSTEMS(B9AI103)/food datatset/runs/detect/yolo_food_detection/weights/best.onnx")
+model = onnx.load("./weights/best.onnx")
 onnx.checker.check_model(model)  # Raises error if invalid
 print("ONNX model is valid!")
